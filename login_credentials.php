136808574a6435e87dc2db0eb553ed1750063a49
Timothy Cole <modesttim@outlook.com>
2015-12-07 02:24:54 -0500

    Ops...

--- login_credentials.php
+++ login_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
-	$username = "ChatBot"; // MLG Username
-	$password = "Czf6boBDf9U9"; // MLG Password
-	$channel = "428"; // MLG Channel ID - If you can't find you channel ID tweet @ModestTim
+	$username = ""; // MLG Username
+	$password = ""; // MLG Password
+	$channel = ""; // MLG Channel ID - If you can't find you channel ID tweet @ModestTim
 	$color = "C0C0C0"; // one of the approved MLG HEX colors
 	$mod = true; // true or false (Channel Owners and Globals not supported)
 
