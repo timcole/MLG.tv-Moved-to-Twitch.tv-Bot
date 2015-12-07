73e9686ad56d31bc767607abf00ef6bcce891855
Timothy Cole <modesttim@outlook.com>
2015-12-07 02:27:31 -0500

    /me Waves goodbye to MLG.tv

--- login_credentials.php
+++ login_credentials.php
@@ -1,11 +1,11 @@
 <?php
 
-	$username = ""; // MLG Username
-	$password = ""; // MLG Password
-	$channel = ""; // MLG Channel ID - If you can't find you channel ID tweet @ModestTim
-	$color = "C0C0C0"; // one of the approved MLG HEX colors
-	$mod = true; // true or false (Channel Owners and Globals not supported)
+$username = ""; // MLG Username
+$password = ""; // MLG Password
+$channel = ""; // MLG Channel ID - If you can't find you channel ID tweet @ModestTim
+$color = "C0C0C0"; // one of the approved MLG HEX colors
+$mod = true; // true or false (Channel Owners and Globals not supported)
 
-	$twitch = "modesttim" // Twitch.TV Username
+$twitch = "modesttim" // Twitch.TV Username
 
 ?>
